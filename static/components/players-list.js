317dd9510f300083acd6664a0d6897982dcb06fe
Sylvain <syl.delabye@gmail.com>
2021-09-25 15:54:01 +0200

    Add loading and check icons during vote

--- static/components/players-list.js
+++ static/components/players-list.js
@@ -4,15 +4,20 @@ Vue.component('players-list', {
       <li class="player" v-for="player in players">
         <span v-if="player.name == name">
           <strong>{{player.name}}</strong>
-          <span class="choice">&nbsp;| {{player.choice}}</span>
+          <span class="choice">&nbsp;&nbsp;{{player.choice}}</span>
         </span>
         <span v-else>
           {{player.name}}
-          <span class="choice">&nbsp;|
-            <span v-if="hideChoices">
-              <span v-if="player.choice" class="hidden">0</span>
+          <span class="choice">&nbsp;
+            <span v-if=player.choice>
+              <span v-if="hideChoices">
+                <i class="fa fa-check fa-fw"></i>
+              </span>
+              <span v-else>{{player.choice}}</span>
             </span>
-            <span v-else>{{player.choice}}</span>
+            <span v-else>
+              <i class="fa fa-spinner fa-pulse fa-fw"></i>
+            </span>
           </span>
         </span>
       </li>
